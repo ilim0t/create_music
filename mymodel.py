7b1735e880f0dbd2781297fbe35d429b7dae5bbe
ilim <ilim0t14@gmail.com>
2018-07-17 17:27:49 +0900

    generator model changed to deep and more conv or lstm.

--- mymodel.py
+++ mymodel.py
@@ -157,24 +157,23 @@ class EmbedID(L.EmbedID):
 
 
 class Res_block(chainer.Chain):
-    def __init__(self, in_channels, out_channels, ksize, activation=F.relu, init_stride=None, stride=1, pad=1):
+    def __init__(self, in_channels, out_channels, ksize, init_stride=None, stride=1, pad=1):
         initializer = chainer.initializers.HeNormal()
-        self.activation = activation
         super(Res_block, self).__init__()
         with self.init_scope():
             # pre-activation
             self.bn1 = L.BatchNormalization(in_channels)
-            self.conv1 = L.ConvolutionND(1 , in_channels, out_channels, ksize, init_stride or stride, pad, initialW=initializer)
+            self.conv1 = L.ConvolutionND(1, in_channels, out_channels, ksize, init_stride or stride, pad, initialW=initializer)
             self.bn2 = L.BatchNormalization(out_channels)
-            self.conv2 = L.ConvolutionND(1 , out_channels, out_channels, ksize, stride, pad, initialW=initializer)
+            self.conv2 = L.ConvolutionND(1, out_channels, out_channels, ksize, stride, pad, initialW=initializer)
             self.bn3 = L.BatchNormalization(out_channels)
 
-            self.xconv = L.ConvolutionND(1 , in_channels, out_channels, 1, stride=2, initialW=initializer)
+            self.xconv = L.ConvolutionND(1, in_channels, out_channels, 1, stride=2, initialW=initializer)
 
     def __call__(self, x, ratio):
         h = self.bn1(x)
         h = self.conv1(h)
-        h = self.activation(self.bn2(h))
+        h = F.leaky_relu(self.bn2(h))
         h = F.dropout(h, ratio)  # Stochastic Depth
         h = self.conv2(h)
         h = self.bn3(h)  # 必要?
@@ -190,29 +189,28 @@ class Res_block(chainer.Chain):
 
 
 class Res_block2(chainer.Chain):
-    def __init__(self, in_channels, out_channels, ksize, activation=F.relu, init_stride=None, stride=1, pad=1):
+    def __init__(self, in_channels, out_channels, ksize, stride=1, pad=1):
         initializer = chainer.initializers.HeNormal()
-        self.activation = activation
         super(Res_block2, self).__init__()
         with self.init_scope():
             # pre-activation
             self.bn1 = L.BatchNormalization(in_channels)
-            self.conv1 = L.DeconvolutionND(1 , in_channels, out_channels, ksize, init_stride or stride, pad, initialW=initializer)
+            self.conv1 = L.DeconvolutionND(1, in_channels, out_channels, ksize, stride, pad, initialW=initializer)
             self.bn2 = L.BatchNormalization(out_channels)
-            self.conv2 = L.DeconvolutionND(1 , out_channels, out_channels, 2*pad+1, stride, pad, initialW=initializer)
+            self.conv2 = L.DeconvolutionND(1, out_channels, out_channels, 3, 1, 1, initialW=initializer)
             self.bn3 = L.BatchNormalization(out_channels)
 
-            self.xdeconv = L.DeconvolutionND(1 , in_channels, out_channels, 2, stride=2, initialW=initializer)
+            self.xdeconv = L.DeconvolutionND(1, in_channels, out_channels, 4, 2, 1, initialW=initializer)
 
     def __call__(self, x, ratio):
         h = self.bn1(x)
         h = self.conv1(h)
-        h = self.activation(self.bn2(h))
+        h = F.relu(self.bn2(h))
         h = F.dropout(h, ratio)  # Stochastic Depth
         h = self.conv2(h)
         h = self.bn3(h)  # 必要?
 
-        if x.shape[2:] != h.shape[2:]:  # skipではないほうのデータの縦×横がこのblock中で小さくなっていた場合skipの方もそれに合わせて小さくする
+        if x.shape[2:] != h.shape[2:]:  # skipではないほうのデータの縦×横がこのblock中で大きくなっていた場合skipの方もそれに合わせて大きくする
             # x = F.average_pooling_2d(x, 1, 2)  # これでいいのか？
             x = self.xdeconv(x)
         if x.shape[1] != h.shape[1]:  # skipではない方のデータのチャンネル数がこのblock内で増えている場合skipの方もそれに合わせて増やす(zero-padding)
@@ -267,25 +265,26 @@ class Generator(chainer.Chain):
         self.n_hidden = n_hidden
         initializer = chainer.initializers.HeNormal()
         with self.init_scope():
-            self.l = L.Linear(32*7)
-            self.bn1 = L.BatchNormalization(8)
+            self.l = L.Linear(64*16)
+            # self.bn1 = L.BatchNormalization(128)
 
-            self.conv1 = L.DeconvolutionND(1, 8, 32, 6, stride=2, pad=2)
+            self.deconv = L.DeconvolutionND(1, 64, 128, 7, stride=2, pad=2)
+            self.lstm = L.NStepBiLSTM(1, 64, 128, 0.1)
 
-            self.block1_1 = Res_block2(32, 32, 3)
-            self.block1_2 = Res_block2(32, 32, 3)
+            self.block1_1 = Res_block2(128, 128, 3)
+            self.block1_2 = Res_block2(128, 128, 3)
 
-            self.block2_1 = Res_block2(32, 64, 4, init_stride=2)
-            self.block2_2 = Res_block2(64, 64, 3)
+            self.block2_1 = Res_block2(128, 128, 6, stride=2, pad=2)
+            self.block2_2 = Res_block2(128, 128, 3)
 
-            self.block3_1 = Res_block2(64, 128, 4, init_stride=2)
+            self.block3_1 = Res_block2(128, 128, 6, stride=2, pad=2)
             self.block3_2 = Res_block2(128, 128, 3)
 
-            self.block4_1 = Res_block2(128, 256, 4, init_stride=2)
+            self.block4_1 = Res_block2(128, 256, 6, stride=2, pad=2)
             self.block4_2 = Res_block2(256, 256, 3)
 
             # self.decoder = L.ConvolutionND(1, 128, n_voc, ksize=1, stride=1)
-            self.decoder2 = L.ConvolutionND(1, 256, 64, ksize=1, stride=1)
+            self.decoder2 = L.ConvolutionND(1, 256, 64, ksize=3, stride=1)
             self.bn2 = L.BatchNormalization(256)
 
     def make_noise(self, batchsize):
@@ -293,26 +292,29 @@ class Generator(chainer.Chain):
 
     def __call__(self, z):
         h = self.l(z)
-        h = F.reshape(h, [h.shape[0], 7, 32])
-        ren = chainer.Variable(np.array([[[i for i in range(1, 1 + h.shape[2])]] * 1] * h.shape[0], dtype=np.float32))
-        h = F.concat([h, ren/h.shape[2]], axis=1)
-        h = self.bn1(h)
+        h = F.reshape(h, [h.shape[0], 64, 16])
+        # ren = chainer.Variable(np.array([[[i for i in range(1, 1 + h.shape[2])]] * 1] * h.shape[0], dtype=np.float32))
+        # h = F.concat([h, ren/h.shape[2]], axis=1)
+        # h = self.bn1(h)
+
+        h = self.deconv(h)
+        # h = F.stack(self.lstm([i for i in h]))
+
+        h = self.block1_1(h, 0)
+        h = self.block1_2(h, 0)
 
         n = 0.3 / 6
-        h = self.conv1(h)
-        h = self.block1_1(h, 1 * n)  # => 64  ×  32
-        h = self.block1_2(h, 2 * n)  # => 64  ×  32
+        h = self.block2_1(h, 1 * n)  # => 128 ×  16
+        h = self.block2_2(h, 2 * n)  # => 128 ×  16
 
-        h = self.block2_1(h, 3 * n)  # => 128 ×  16
-        h = self.block2_2(h, 4 * n)  # => 128 ×  16
-
-        h = self.block3_1(h, 5 * n)  # => 256 ×   8
-        h = self.block3_2(h, 6 * n)  # => 256 ×   8
+        h = self.block3_1(h, 3 * n)  # => 256 ×   8
+        h = self.block3_2(h, 4 * n)  # => 256 ×   8
 
         h = self.block4_1(h, 5 * n)  # => 256 ×   8
         h = self.block4_2(h, 6 * n)  # => 256 ×   8
 
-        h = self.decoder2(self.bn2(h))
+        # h = self.bn2(h)
+        h = self.decoder2(h)
         return h
         # h = self.decoder(h)
         # # h = self.bn(h)
@@ -333,17 +335,17 @@ class Discriminator(chainer.Chain):
             self.bn1 = L.BatchNormalization(n_vec)
             self.conv1 = L.ConvolutionND(1, n_vec, 64, 7, stride=2, pad=3)
 
-            self.block1_1 = Res_block(64, 64, 3, activation=F.leaky_relu, pad=1)
-            self.block1_2 = Res_block(64, 64, 3, activation=F.leaky_relu, pad=1)
+            self.block1_1 = Res_block(64, 64, 3)
+            self.block1_2 = Res_block(64, 64, 3)
 
-            self.block2_1 = Res_block(64, 128, 3, activation=F.leaky_relu, init_stride=2, pad=1)
-            self.block2_2 = Res_block(128, 128, 3, activation=F.leaky_relu, pad=1)
+            self.block2_1 = Res_block(64, 128, 3, init_stride=2)
+            self.block2_2 = Res_block(128, 128, 3)
 
-            self.block3_1 = Res_block(128, 64, 3, activation=F.leaky_relu, init_stride=2, pad=1)
-            self.block3_2 = Res_block(64, 64, 3, activation=F.leaky_relu, pad=1)
+            self.block3_1 = Res_block(128, 64, 3, init_stride=2)
+            self.block3_2 = Res_block(64, 64, 3)
 
-            self.block4_1 = Res_block(64, 32, 3, activation=F.leaky_relu, init_stride=2, pad=1)
-            self.block4_2 = Res_block(32, 32, 3, activation=F.leaky_relu, pad=1)
+            self.block4_1 = Res_block(64, 32, 3, init_stride=2)
+            self.block4_2 = Res_block(32, 32, 3)
 
             self.bn2 = L.BatchNormalization(32)
             self.l = L.Linear(1, initialW=initializer)
@@ -360,7 +362,7 @@ class Discriminator(chainer.Chain):
         else:
             h = self.sequence_embed(x)
 
-        h = self.bn1(h)
+        # h = self.bn1(h)
         h = self.conv1(h)
 
         n = 0.5 / 8
@@ -377,7 +379,7 @@ class Discriminator(chainer.Chain):
         h = self.block4_2(h, 8 * n)  # => 256 ×   8
 
         # h = F.average_pooling_nd(h, h.shape[2])  # global average pooling
-        h = self.bn2(h)
+        # h = self.bn2(h)
         h = F.spatial_pyramid_pooling_2d(F.expand_dims(h, axis=3), 2, F.MaxPooling2D)
         h = F.reshape(h, h.shape[:2])
         return self.l(F.leaky_relu(h))  # 正: 正解由来
